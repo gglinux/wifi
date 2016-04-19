2938785bdc3c57118b1953223a213ca082185c62
gglinux <gglinux@163.com>
2016-04-19 18:12:33 +0800

    完成wordcount

---
+++ WordCount/src/gjw/hadoop/study/WordCount.java
@@ -0,0 +1,69 @@
+package gjw.hadoop.study;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+
+
+public class WordCount {
+	//inner class Mapper
+	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> 
+	{
+		//map job value,Text type
+		private Text word = new Text();
+		private final static IntWritable one = new IntWritable(1);
+		//map input type,Object and Text,output type,Text and IntWritable
+		public void map(Object key, Text value, Context context ) throws IOException, InterruptedException {
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while(itr.hasMoreTokens()) {
+				//get the word, store in Text
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
+		}
+	}
+	//inner class Reducer
+	public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> 
+	{
+		private IntWritable result = new IntWritable();
+		public void reduce(Text key, Iterable<IntWritable> values, Context context ) throws IOException, InterruptedException {
+			int sum = 0;
+			for(IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf,args).getRemainingArgs();
+		if(otherArgs.length != 2) {
+			System.out.println("Usage:wordcount <in> <out>");
+			System.exit(2);
+		}
+		Job job = new Job(conf, "word count");
+		job.setJarByClass(WordCount.class);
+		job.setMapperClass(TokenizerMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		
+	}
+}
